f50020c906a5a9ddc71d9263e43cca17aa3ee36b
pronab69 <pronabdebnath35@gmail.com>
2025-08-16 12:54:58 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Nav from './components/Nav';
 import Middle from './components/Middle';
 import Breakmar from './components/Breakmar';
-import SwiperMenu from './Slide';
+
 
 const App = () => {
   return (
 
 
     <> 
-    <SwiperMenu></SwiperMenu>  
+   
      <Nav></Nav>
           <div className='w-10/12 flex items-center mt-2 mx-auto bg-base-300'>
             
